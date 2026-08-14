73445a879b461d51c3d9b37e1b5d18656812e34f
hopeforfuture <mnbl87@gmail.com>
2026-08-14 12:45:46 +0530

    submit quiz and calculate marks

--- src/main/java/com/telusko/quizapp/controller/QuizController.java
+++ src/main/java/com/telusko/quizapp/controller/QuizController.java
@@ -1,13 +1,16 @@
 package com.telusko.quizapp.controller;
 
+import com.telusko.quizapp.dto.QuizResponse;
+import com.telusko.quizapp.dto.QuizResultResponse;
+import com.telusko.quizapp.model.Question;
+import com.telusko.quizapp.model.QuizAnswer;
 import com.telusko.quizapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/quiz")
@@ -20,8 +23,20 @@ public class QuizController {
     public ResponseEntity<String> createQuiz(@RequestParam("category") String category,
                                              @RequestParam("numQ") int numQ,
                                              @RequestParam("title") String title) {
-
         return quizService.createQuiz(category, numQ, title);
-
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<QuizResponse>> getQuizQuestions(@PathVariable("id") Integer id) {
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<QuizResultResponse> submitQuiz(
+            @PathVariable Integer id,
+            @RequestBody List<QuizAnswer> quizAnswers) {
+
+        return quizService.calculateResult(id, quizAnswers);
+    }
+
 }
--- src/main/java/com/telusko/quizapp/service/QuizService.java
+++ src/main/java/com/telusko/quizapp/service/QuizService.java
@@ -2,14 +2,19 @@ package com.telusko.quizapp.service;
 
 import com.telusko.quizapp.dao.QuestionDao;
 import com.telusko.quizapp.dao.QuizDao;
+import com.telusko.quizapp.dto.QuizResponse;
+import com.telusko.quizapp.dto.QuizResultResponse;
 import com.telusko.quizapp.model.Question;
 import com.telusko.quizapp.model.Quiz;
+import com.telusko.quizapp.model.QuizAnswer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -28,5 +33,44 @@ public class QuizService {
         quizDao.save(quiz);
 
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuizResponse>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuizResponse> questionsForUser = new ArrayList<>();
+
+        for(Question q : questionsFromDB) {
+            QuizResponse qr = new QuizResponse(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionsForUser.add(qr);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+    }
+
+    public ResponseEntity<QuizResultResponse> calculateResult(
+            Integer id,
+            List<QuizAnswer> quizAnswers) {
+
+        Quiz quiz = quizDao.findById(id).get();
+        int right = 0;
+        int i = 0;
+
+        List<Question> questions = quiz.getQuestions();
+
+        for (QuizAnswer quizAnswer : quizAnswers) {
+            if (quizAnswer.getResponse()
+                    .equals(questions.get(i).getRightAnswer())) {
+                right++;
+            }
+            i++;
+        }
+
+        QuizResultResponse response =
+                new QuizResultResponse(
+                        "Quiz submitted successfully",
+                        right
+                );
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
